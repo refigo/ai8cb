a771aacab7f395622f59dbccae13521b2d0d6d01
Refi Go <64242999+refigo@users.noreply.github.com>
2024-04-01 14:27:46 +0900

    Feat: Implement tokenizer

---
+++ common.py
@@ -0,0 +1,31 @@
+
+# Use GPT2TokenizerFast to tokenize the text
+def toknize_gpt2(text):
+	from transformers import GPT2TokenizerFast
+	tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+	return tokenizer.encode(text)
+
+def download(url):
+	import requests
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'
+	}
+	response = requests.get(url, headers=headers)
+	response.encoding = 'UTF-8'
+	return response
+
+def get_text_from_html(html):
+	from bs4 import BeautifulSoup
+	soup = BeautifulSoup(html.text, 'html.parser')
+	return soup.get_text()
+
+def split_text(tokenized, budget):
+	split = []
+	for i in range(0, len(tokenized), budget):
+		split.append(tokenized[i:i+budget])
+	
+	if len(split[-1]) < budget and len(split) > 1:
+		remaining_budget = budget - len(split[-1])
+		split[-1] = split[-2][:remaining_budget] + split[-1]
+
+	return split
--- copywriter.py
+++ copywriter.py
@@ -1,11 +1,20 @@
 import os
 from openai import OpenAI
 import sys
+from common import download, get_text_from_html, toknize_gpt2, split_text
+
+MAX_TOKENS = 4000
+MAX_NEW_TOKENS = 100
+
+PREPROMPT = "SUMMARIZE THE FOLLOWING DOCUMENT IN KOREAN \n=====\n"
+POSTPROMPT = "\n=====\nSUMMARY:\n"
+
+TOKEN_BUDGET = MAX_TOKENS - len(toknize_gpt2(PREPROMPT)) - len(toknize_gpt2(POSTPROMPT)) - MAX_NEW_TOKENS
 
 api_key = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
-def openai_inference_gpt3_turbo(prompt: str, max_tokens: int = 100):
+def openai_inference_gpt3_5_turbo(prompt: str, max_tokens: int = MAX_NEW_TOKENS):
 	response = client.chat.completions.create(
 		model="gpt-3.5-turbo",
 		messages=[
@@ -17,33 +26,26 @@ def openai_inference_gpt3_turbo(prompt: str, max_tokens: int = 100):
 	)
 	return response.choices[0].message
 
-def inference(prompt, max_tokens=100):
+def inference(prompt, max_tokens=MAX_NEW_TOKENS):
 	return openai_inference_gpt3_turbo(prompt, max_tokens)
 
-# print(inference("Hi, how are you? Answer using korean: "))
+def summarize(text):
+	prompt = "SUMMARIZE THE FOLLOWING DOCUMENT IN KOREAN \n=====\n" + text + "\n=====\nSUMMARY:\n"
+	return inference(prompt)
 
 
-def download(url):
-	import requests
-	headers = {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'
-	}
-	response = requests.get(url, headers=headers)
-	response.encoding = 'UTF-8'
-	return response
-
-def get_text_from_html(html):
-	from bs4 import BeautifulSoup
-	soup = BeautifulSoup(html.text, 'html.parser')
-	return soup.get_text()
-
-# Take url from the first argument
 url = sys.argv[1]
-
 html = download(url)
-
 text = get_text_from_html(html)
 
-print(text)
+tokenized = toknize_gpt2(text)
+
+split = split_text(tokenized, TOKEN_BUDGET)
+
+# result = summarize(text)
+
+print(split)
+
+print([len(x) for x in split])
 
 
